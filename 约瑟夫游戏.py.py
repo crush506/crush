1d614359d4e44a177fe31b3f0a8b07d2b8899c01
crush506 <73268873+crush506@users.noreply.github.com>
2020-11-30 18:33:22 +0800

    Add files via upload

---
+++ 约瑟夫游戏.py.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def move(players,step):
+    num=step-1
+    while num>0:
+        tmp=players.pop(0)
+        players.append(tmp)
+        num=num-1
+    return players
+def play(players,step,alive):
+    list1=[i for i in range(1,players+1)]
+    while len(list1)>alive:
+        list1=move(list1,step)
+        list1.pop(0)
+    return list1
+players_num=input("请输入参加游戏的人数")
+step_num=("请输入淘汰的数字")
+alive_num=input("请输入幸存人数")
+alive_list=play(players_num,step_num,alive_num)
+print(alive_list)
+while len(list1)>alive:
+    num=step-1
+    while num>0:
+        tmp=list1.pop(0)
+        list1.append(tmp)
+        num=num-1
+    list1=move(list1,step)
+    list1.pop(0)
+return list1
+
+
+# In[ ]:
+
+
+
+
